cfc3a9c4b4b3b9ddf7ffe2ea22d6c830ae579841
Killianp-dev <killian.pasche@gmail.com>
2025-05-17 17:14:14 +0200

    allowed_hosts update

--- src/config/settings.py
+++ src/config/settings.py
@@ -29,9 +29,9 @@ environ.Env.read_env(BASE_DIR.parent / '.env')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ai-technews.fr', 'www.ai-technews.fr', '35.180.44.75', '127.0.0.1']
 
 # mail factice console
 
