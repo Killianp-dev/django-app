c7a9ce0ad15bf485e2e48b120cefe83aafe6e63d
Killianp-dev <killian.pasche@gmail.com>
2025-05-17 21:28:47 +0200

    css update

--- src/config/settings.py
+++ src/config/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env(BASE_DIR.parent / '.env')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['ai-technews.fr', 'www.ai-technews.fr', '35.180.44.75', '127.0.0.1']
 
