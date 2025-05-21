7795fff781830c61a5028ff6c8f436a10d6ab38e
Killianp-dev <killian.pasche@gmail.com>
2025-05-22 00:23:33 +0200

    Fix DEBUG setting

--- src/config/settings.py
+++ src/config/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env(BASE_DIR.parent / '.env')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = ['ai-technews.fr', 'www.ai-technews.fr', '35.180.44.75', '127.0.0.1']
 
